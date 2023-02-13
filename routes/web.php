8fcfa1b15322b0e0326d073b4322a66a8c3a4b9b
novanmasriandi11 <novanmasriandi@gmail.com>
2023-02-14 04:43:40 +0700

    backend project kaimono store

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProductGalleryController;
+use App\Http\Controllers\TransactionController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('/',[DashboardController::class,'index'])->name('dashboard');
+Auth::routes(['register' => false]);
+
+Route::get('products/{id}/gallery', [ProductController::class, 'gallery'])->name('products.gallery');
+Route::resource('products', ProductController::class);
+Route::resource('product-galleries', ProductGalleryController::class);
+Route::get('transaction/{id}/set-status', [TransactionController::class, 'setStatus'])->name('transactions.status');
+Route::resource('transactions', TransactionController::class);
